97fb39be4bf7292592153ae3e20b9505270fa3a4
lei <25927480+yiyi17@users.noreply.github.com>
2022-10-28 09:20:05 +0800

    demo: test

--- demo/test.js
+++ demo/test.js
@@ -1,7 +1,23 @@
+const path = require("path");
 const ts = require("../built/local/typescript");
 
-const filename = "./input.ts";
+const filename = path.join(__dirname, "./input.ts");
 const program = ts.createProgram([filename], {
     allowJs: false
 });
 const sourceFile = program.getSourceFile(filename);
+const typeChecker = program.getTypeChecker();
+
+function visitNode(node) {
+    if (node.kind === ts.SyntaxKind.TypeReference)  {
+        const type = typeChecker.getTypeFromTypeNode(node);
+
+        debugger;
+    }
+
+    node.forEachChild(child =>
+        visitNode(child)
+    );
+}
+
+visitNode(sourceFile);
